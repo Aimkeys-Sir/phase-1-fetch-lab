5894b4ed111d04ac9e5e901166cd5657919ee00c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-15 19:53:55 +0300

    commit tree ebfb86b577f8a667fd396c224bb7d65c7e90256e

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  // To pass the tests, don't forget to 
+  //return your fetch!
+return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(json=>renderBooks(json))
+    
+
   
 }
 
@@ -12,6 +18,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
